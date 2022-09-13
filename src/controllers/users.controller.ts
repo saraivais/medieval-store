f191fc404602c37f245e18235b5e7020d366d22a
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 17:58:49 -0300

    feat [usersController]: creates basic controller structure & imports

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+import usersService from '../services/users.service';
+import User from '../interfaces/user.interface';
+
+const usersController = {
+
+};
+
+export default usersController;
