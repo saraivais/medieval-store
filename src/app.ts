db21fb4b894c57976866446079b790c9875bb180
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 19:29:04 -0300

    feat [app]: imports and uses ordersRoute

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import productsRoute from './routes/products.route';
 import usersRoute from './routes/users.route';
+import ordersRoute from './routes/orders.route';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productsRoute);
 app.use('/users', usersRoute);
+app.use('/orders', ordersRoute);
 
 export default app;
