6ff5f7396a6641b5855083c8bdf4db6ad76c8b06
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 17:07:42 -0300

    feat [productsRoute]: creates post endpoint~

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -4,5 +4,6 @@ import productsController from '../controllers/products.controller';
 const productsRoute = express.Router();
 
 productsRoute.get('/', productsController.getAll);
+productsRoute.post('/', productsController.create);
 
 export default productsRoute;
