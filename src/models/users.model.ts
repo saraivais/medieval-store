d8ff3bfc18fab4dce8a73dcf21160b3980d54414
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 17:27:27 -0300

    feat [usersModel]: adds create function~

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,7 +1,13 @@
+import { ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
+import User from '../interfaces/user.interface';
 
 const usersModel = {
-
+  create: async ({ username, classe, level, password }:User):Promise<void> => {
+    const query = `INSERT INTO Trybesmith.Users 
+    (username, classe, level, password) VALUES (?, ?, ?, ?);`;
+    await connection.execute<ResultSetHeader>(query, [username, classe, level, password]);
+  },
 };
 
 export default usersModel;
