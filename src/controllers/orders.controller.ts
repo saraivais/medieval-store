4123f0efb520d5f22b25b2512a782b74427a9837
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 19:28:47 -0300

    feat [ordersController]: creates basic structure for model development~

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,7 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
 import ordersService from '../services/orders.service';
 
 const ordersController = {
-
+  getAll: async (_request: Request, response: Response, _next: NextFunction):Promise<Response> => {
+    await ordersService.getAll();
+    return response.status(200).json({ message: 'XABLAU' });
+  },
 };
 
 export default ordersController;
