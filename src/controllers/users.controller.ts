b29ef39fd931ab8fd6885e9a7c1ad317ded8f4a0
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 18:00:56 -0300

    feat [usersController]: adds create middleware~

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -3,7 +3,11 @@ import usersService from '../services/users.service';
 import User from '../interfaces/user.interface';
 
 const usersController = {
-
+  create: async (request: Request, response: Response, _next: NextFunction):Promise<Response> => {
+    const userToCreate: User = request.body;
+    const result = await usersService.create(userToCreate);
+    return response.status(201).json(result);
+  },
 };
 
 export default usersController;
