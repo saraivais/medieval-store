7d5a2510d911bc080c59fe24661f1bd89ea1bba2
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 17:06:26 -0300

    feat [productsController]: adds create middleware~

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,10 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 import productsService from '../services/products.service';
+import Product from '../interfaces/product.interface';
 
 const productsController = {
   getAll: async (_request: Request, response: Response, _next: NextFunction):Promise<Response> => {
     const allProducts = await productsService.getAll();
     return response.status(200).json(allProducts);
+  },
+
+  create: async (request: Request, response: Response, _next: NextFunction):Promise<Response> => {
+    const productToCreate: Product = request.body;
+    const createdProduct = await productsService.create(productToCreate);
+    return response.status(201).json(createdProduct);
   },
 };
 
