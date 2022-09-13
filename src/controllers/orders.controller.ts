92a252f5444fb586985c402f91cda156349805b4
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 19:59:36 -0300

    feat [ordersService]: adds types and return from service~

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -3,8 +3,8 @@ import ordersService from '../services/orders.service';
 
 const ordersController = {
   getAll: async (_request: Request, response: Response, _next: NextFunction):Promise<Response> => {
-    await ordersService.getAll();
-    return response.status(200).json({ message: 'XABLAU' });
+    const allOrders = await ordersService.getAll();
+    return response.status(200).json(allOrders);
   },
 };
 
