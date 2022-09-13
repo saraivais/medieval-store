741ac67106444bb97137679634fa83aa37a9f319
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 19:28:34 -0300

    feat [ordersService]: creates basic structure for model development~

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,7 +1,9 @@
 import ordersModel from '../models/orders.model';
 
 const ordersService = {
-  
+  getAll: async ():Promise<void> => {
+    await ordersModel.getAll();
+  },
 };
 
 export default ordersService;
