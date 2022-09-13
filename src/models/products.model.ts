2d3837f839b3ca650a75867ac9ec55998d31ceba
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 16:56:39 -0300

    feat [productsModel]: adds create function~

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,4 +1,4 @@
-import { RowDataPacket } from 'mysql2/promise';
+import { RowDataPacket, ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
 import Product from '../interfaces/product.interface';
 
@@ -9,6 +9,12 @@ const productsModel = {
     const query = 'SELECT * FROM Trybesmith.Products';
     const [allProducts] = await connection.execute<ProductDataPacket[]>(query);
     return allProducts;
+  },
+
+  create: async ({ name, amount }:Product):Promise<Product> => {
+    const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?);';
+    const [{ insertId }] = await connection.execute<ResultSetHeader>(query, [name, amount]);
+    return { id: insertId, name, amount };
   },
 };
 
