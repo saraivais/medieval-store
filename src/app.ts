590531cab699964c7c88d519cf0b5b27d2344257
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 16:26:39 -0300

    feat [app]: imports & uses productRoute~

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
+import productsRoute from './routes/products.route';
 
 const app = express();
 
 app.use(express.json());
+app.use('/products', productsRoute);
 
 export default app;
