720b6ca85dc1ef97f16fb705c176f5f2c3b19ad6
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 17:54:37 -0300

    feat [usersService]: creates token generation function~

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,8 +1,26 @@
+import dotenv from 'dotenv';
+import jwt, { SignOptions } from 'jsonwebtoken';
 import usersModel from '../models/users.model';
 import User from '../interfaces/user.interface';
+import Token from '../interfaces/token.interface';
+
+dotenv.config();
+
+const jwtSecret = process.env.JWT_SECRET || 'Unbowed, Unbent, Unbroken';
 
 const usersService = {
+  create: async (userToCreate:User):Promise<Token> => {
+    
+  },
 
+  generateToken: (username:string, id: number):string => {
+    const jwtConfig:SignOptions = {
+      algorithm: 'HS256',
+    };
+
+    const token = jwt.sign({ data: { username, id } }, jwtSecret, jwtConfig);
+    return token;
+  },
 };
 
 export default usersService;
