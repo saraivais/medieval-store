71843d04a8cb6cfbb824500b38471fe46e7f7df0
saraivais <isadorasaraiva95@gmail.com>
2022-09-13 18:07:20 -0300

    feat [userRoute]: creates route & adds post endpoint

---
+++ src/routes/users.route.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import usersController from '../controllers/users.controller';
+
+const usersRoute = express.Router();
+
+usersRoute.post('/', usersController.create);
+
+export default usersController;
