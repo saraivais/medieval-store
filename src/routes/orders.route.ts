d7e1795bf75fdd2b9a70cb780bd7aad35fa4e555
saraivais <isadorasaraiva95@gmail.com>
2022-09-18 17:52:42 -0300

    feat [ordersRoute]: adds create endpoint~

--- src/routes/orders.route.ts
+++ src/routes/orders.route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import ordersController from '../controllers/orders.controller';
+import jwtValidation from '../middlewares/jwtValidation';
 
 const ordersRoute = express.Router();
 
 ordersRoute.get('/', ordersController.getAll);
+ordersRoute.post('/', jwtValidation, ordersController.create);
 
 export default ordersRoute;
