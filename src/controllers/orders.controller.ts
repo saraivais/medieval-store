56ec5334ccfc363e38dec80af08fc631e64d45f6
saraivais <isadorasaraiva95@gmail.com>
2022-09-18 17:52:20 -0300

    feat [ordersController]: adds create middleware~

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,10 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 import ordersService from '../services/orders.service';
+import Token from '../interfaces/token.interface';
+import Order from '../interfaces/order.interface';
 
 const ordersController = {
   getAll: async (_request: Request, response: Response, _next: NextFunction):Promise<Response> => {
     const allOrders = await ordersService.getAll();
     return response.status(200).json(allOrders);
+  },
+
+  create: async (request: Request, response: Response, _next: NextFunction) => {
+    const jwtToken = {
+      token: request.headers.authorization,
+    };
+    const productsToInsert:Order = request.body;    
+    const result = await ordersService.create(jwtToken as Token, productsToInsert);
+    return response.status(201).json(result);
   },
 };
 
