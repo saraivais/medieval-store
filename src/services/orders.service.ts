f759ae769baf667ebb7a548471cfe425d6936f14
saraivais <isadorasaraiva95@gmail.com>
2022-09-18 17:51:17 -0300

    feat [ordersService]: adds create function & edit joi

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,6 +1,10 @@
 import Joi from 'joi';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import ordersModel from '../models/orders.model';
 import Order from '../interfaces/order.interface';
+import Token from '../interfaces/token.interface';
+// import Product from '../interfaces/product.interface';
+import productsModel from '../models/products.model';
 
 const ordersService = {
   getAll: async ():Promise<Order[]> => {
@@ -8,15 +12,38 @@ const ordersService = {
     return allOrders;
   },
 
-  create: async () => {},
+  create: async (token: Token, productsToInsert:Order):Promise<Order> => {
+    const { error } = ordersService.validateOrderData.validate(productsToInsert);
+    if (error) {
+      throw new Error(error.details[0].message);
+    } 
+    const { data } = ordersService.getUserId(token.token);
+
+    const orderId = await ordersModel.create(data.id);
+
+    await Promise.all(productsToInsert.productsIds
+      .map((id) => productsModel.editOrder({ id, orderId })));
+
+    return {
+      userId: data.id,
+      productsIds: productsToInsert.productsIds,
+    };
+  },
+
+  getUserId: (token:string):JwtPayload => {
+    const decodedPayload = jwt.decode(token);
+    return decodedPayload as JwtPayload;
+  },
 
   validateOrderData: Joi.object({
-    productsIds: Joi.array().required().items(Joi.number().messages({
+    productsIds: Joi.array().required().min(1).items(Joi.number().messages({
       'number.base': '422|"productsIds" must include only numbers',
-    })).messages({
-      'any.required': '400|"productsIds" is required',
-      'array.base': '422|"productsIds" must be an array',
-    }),
+    }))
+      .messages({
+        'any.required': '400|"productsIds" is required',
+        'array.base': '422|"productsIds" must be an array',
+        'array.min': '422|"productsIds" must include only numbers',
+      }),
   }),
 };
 
