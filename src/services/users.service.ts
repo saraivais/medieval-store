cc138c6731a909c0cca7cbdb7fda5c2c2dd3e149
saraivais <isadorasaraiva95@gmail.com>
2022-09-18 16:06:15 -0300

    feat [usersService]: adds joi validaiton schema for user info

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -10,6 +10,29 @@ dotenv.config();
 const jwtSecret = process.env.JWT_SECRET || 'Unbowed, Unbent, Unbroken';
 
 const usersService = {
+  validateUserData: Joi.object({
+    username: Joi.string().required().min(3).messages({
+      'any.required': '400|"username" is required',
+      'string.base': '422|"username" must be a string',
+      'string.min': '422|"username" length must be at least 3 characters long',
+    }),
+    classe: Joi.string().required().min(3).messages({
+      'any.required': '400|"classe" is required',
+      'string.base': '422|"classe" must be a string',
+      'string.min': '422|"classe" length must be at least 3 characters long',
+    }),
+    level: Joi.number().required().greater(1).messages({
+      'any.required': '400|"level" is required',
+      'number.base': '422|"level" must be a number',
+      'number.greater': '422|"level" must be greater than or equal to 1',
+    }),
+    password: Joi.string().required().min(8).messages({
+      'any.required': '400|"password" is required',
+      'string.base': '422|"password" must be a string',
+      'string.min': '422|"password" length must be at least 8 characters long',
+    }),
+  }),
+
   create: async (userToCreate:User):Promise<Token> => {
     const createdId = await usersModel.create(userToCreate);
     const token = usersService.generateToken(userToCreate.username, createdId);
