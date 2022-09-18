83bd0a24349ba3339fe58f83dce61ff96d49e5ea
saraivais <isadorasaraiva95@gmail.com>
2022-09-18 15:55:53 -0300

    feat [productsService]: adds joi schema for body validation~

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -11,6 +11,19 @@ const productsService = {
     const createdProduct = await productsModel.create(productToCreate);
     return createdProduct;
   },
+
+  validateProductData: Joi.object({
+    name: Joi.string().required().min(3).messages({
+      'any.required': '400|"name" is required',
+      'string.base': '422|"name" must be a string',
+      'string.min': '422|"name" length must be at least 3 characters long',
+    }),
+    amount: Joi.string().required().min(3).messages({
+      'any.required': '400|"amount" is required',
+      'string.base': '422|"amount" must be a string',
+      'string.min': '422|"amount" length must be at least 3 characters long',
+    }),
+  }),
 };
 
 export default productsService;
