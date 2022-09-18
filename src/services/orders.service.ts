a48a8ce10ee10f870b057b7356ece865abd09cee
saraivais <isadorasaraiva95@gmail.com>
2022-09-18 16:26:04 -0300

    feat [ordersService]: creates joi schema for productsIds

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,3 +1,4 @@
+import Joi from 'joi';
 import ordersModel from '../models/orders.model';
 import Order from '../interfaces/order.interface';
 
@@ -6,6 +7,17 @@ const ordersService = {
     const allOrders = await ordersModel.getAll();
     return allOrders;
   },
+
+  create: async () => {},
+
+  validateOrderData: Joi.object({
+    productsIds: Joi.array().required().items(Joi.number().messages({
+      'number.base': '422|"productsIds" must include only numbers',
+    })).messages({
+      'any.required': '400|"productsIds" is required',
+      'array.base': '422|"productsIds" must be an array',
+    }),
+  }),
 };
 
 export default ordersService;
