10ca3feb6fb4841f5accd1c279f781e4881fb6f2
saraivais <isadorasaraiva95@gmail.com>
2022-09-18 15:41:29 -0300

    feat [app]: imports errorMiddleware & loginRoute

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import 'express-async-errors';
 import productsRoute from './routes/products.route';
 import usersRoute from './routes/users.route';
 import ordersRoute from './routes/orders.route';
+import loginRoute from './routes/login.route';
+import errorMiddleware from './middlewares/errorMiddleware';
 
 const app = express();
 
@@ -10,5 +12,8 @@ app.use(express.json());
 app.use('/products', productsRoute);
 app.use('/users', usersRoute);
 app.use('/orders', ordersRoute);
+app.use('/login', loginRoute);
+
+app.use(errorMiddleware);
 
 export default app;
