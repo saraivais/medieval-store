b66e3e8f2a40d6f2736ad616467b427818760a59
saraivais <isadorasaraiva95@gmail.com>
2022-09-18 16:01:17 -0300

    fix [usersService]: removes empty validation from joi~

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -27,13 +27,11 @@ const usersService = {
   },
 
   validateLoginData: Joi.object({
-    username: Joi.string().required().empty('').messages({
+    username: Joi.string().required().messages({
       'any.required': '400|"username" is required',
-      'string.empty': '400|"username" is required',
     }),
-    password: Joi.string().required().empty('').messages({
+    password: Joi.string().required().messages({
       'any.required': '400|"password" is required',
-      'string.empty': '400|"password" is required',
     }),
   }),
   
